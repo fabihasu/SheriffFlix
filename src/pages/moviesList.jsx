bd157ec4b1d209bf68c3dc2ab81c27e3c6205ddd
Fabiana Hasu <fabi.hasu@gmail.com>
2024-03-10 23:00:59 -0300

    paginador

--- src/pages/moviesList.jsx
+++ src/pages/moviesList.jsx
@@ -1,24 +1,48 @@
 import React,{useState,useEffect} from "react";
 import Movies from "../components/movies";
+import "./PaginationButtons.css";
 
 function MoviesList() {
     const [dados, setDados] = useState(null);
+    const [currentPage, setCurrentPage] = useState(1);
+
     useEffect(() => {
       const buscarDados = async () => {
-        const resposta = await fetch('http://localhost:3006/movies')
+        const resposta = await fetch(`http://localhost:3006/movies?page=${currentPage}`)
         const dados = await resposta.json();
         setDados(dados.results);
       };
   
       buscarDados();
-    }, []); 
+    }, [currentPage]); 
   
+    const handleNextPage = () => {
+      setCurrentPage((prevCurrentPage) => prevCurrentPage + 1);
+    };
+  
+    const handlePreviousPage = () => {
+      setCurrentPage((prevCurrentPage) => Math.max(prevCurrentPage - 1, 1));
+    };
+
     if (!dados || dados.length === 0) {
-      return <div>Carregando...</div>; 
+      return <div>Cargando...</div>; 
     } 
   
     return (
       <div>
+        <div className="pagination-container">
+          <button
+            onClick={handlePreviousPage}
+            disabled={currentPage === 1}
+            className="pagination-btn btn-previous"
+          > Anterior 
+          </button>
+          <button
+            onClick={handleNextPage}
+            className="pagination-btn btn-next"
+          > Siguiente 
+          </button>
+        </div>
         <div className="movie-gallery">
           {dados.map((movie, index) => (
             <Movies 
@@ -31,6 +55,19 @@ function MoviesList() {
             rating={movie.vote_average} 
             />
           ))}
+        </div>
+        <div className="pagination-container">
+          <button
+            onClick={handlePreviousPage}
+            disabled={currentPage === 1}
+            className="pagination-btn btn-previous"
+          > Anterior 
+          </button>
+          <button
+            onClick={handleNextPage}
+            className="pagination-btn btn-next"
+          > Siguiente 
+          </button>
         </div>
       </div>
     );
